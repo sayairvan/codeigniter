0ca22491bc3b2e8f652923c1dc4bc6c016e531cc
putridya <putridya99@gmail.com>
2020-05-09 20:00:33 +0700

    login terbaru

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -11,6 +11,7 @@
                     <div class="row">
                         <!-- <div class="col-lg-6 d-none d-lg-block bg-login-image"></div> -->
                         <div class="col-lg">
+                            <!-- coba -->
                             <div class="p-5">
                                 <div class="text-center">
                                     <h1 class="h4 text-gray-900 mb-4">Login Page</h1>
